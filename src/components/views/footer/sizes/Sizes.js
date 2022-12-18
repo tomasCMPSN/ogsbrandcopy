81259700147000a7f22c47bf9feb5d1fff2eef5d
Tomas Campusano <tomascampusano.10@gmail.com>
2022-12-18 02:49:38 -0300

    Added: content in Sizes

--- src/components/views/footer/sizes/Sizes.js
+++ src/components/views/footer/sizes/Sizes.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React from "react";
+import { Container, Image } from "react-bootstrap";
+import { StyledContainer } from "../../auth/StyledContainer";
 
 const Sizes = () => {
-    return (
-        <Container style={{ marginTop: "16vh" }}>
-            Sizes
-        </Container>
-    );
+  return (
+    <Container style={{ marginTop: "16vh" }}>
+      <StyledContainer>
+        <h1 className="fw-bold pt-5">SIZES</h1>
+      </StyledContainer>
+      <Image
+        fluid={true}
+        src="https://ogsbrand.storage.googleapis.com/media/original_images/TALLAS_-_OGS.jpg"
+        alt="Sizes for top"
+      />
+      <Image
+        fluid={true}
+        src="https://ogsbrand.storage.googleapis.com/media/original_images/TALLAS_-_OGS1.jpg"
+        alt="Sizes for bottom"
+      />
+    </Container>
+  );
 };
 
 export default Sizes;

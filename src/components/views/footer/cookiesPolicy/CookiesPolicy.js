192bc17db10fc851fbd6ac71aaa2e5e3447166c4
Tomas Campusano <tomascampusano.10@gmail.com>
2022-12-18 02:11:22 -0300

    Added: content in CookiesPolicy

--- src/components/views/footer/cookiesPolicy/CookiesPolicy.js
+++ src/components/views/footer/cookiesPolicy/CookiesPolicy.js
@@ -1,12 +1,161 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React from "react";
+import { Container } from "react-bootstrap";
+import { StyledContainer } from "../../auth/StyledContainer";
 
 const CookiePolicy = () => {
-    return (
-        <Container style={{ marginTop: "16vh" }}>
-            CookiePolicy
-        </Container>
-    );
+  return (
+    <Container style={{ marginTop: "16vh" }}>
+      <StyledContainer>
+        <div>
+          <h1 className="fw-bold pt-5">COOKIES POLICY</h1>
+        </div>
+        <div className="py-4">
+          <p>1. WHAT ARE COOKIES?</p>
+          <p>
+            Cookies are small data files that are received on the terminal from
+            the visited website and are used to record certain browsing
+            interactions on a website by storing data that can be updated and
+            retrieved. These files are stored on the user's computer and contain
+            anonymous data that are not harmful to your computer. They are used
+            to remember the user's preferences, such as the selected language,
+            access data or personalization of the page.
+          </p>
+          <p>
+            Cookies can also be used to record anonymous information about how a
+            visitor uses a site. For example, from which web page you accessed
+            the site, or whether you used a banner advertisement to get there.
+          </p>
+        </div>
+        <div>
+          <p>1. WHY DO WE USE COOKIES?</p>
+          <p>
+            LEEOSMERCH, SL and VIZZMEDIA ONLINE, SL use cookies that are
+            strictly necessary and essential for you to use our websites and
+            allow you to move freely, use secure areas, personalized options,
+            etc. In addition, LEEOSMERCH, SL and VIZZMEDIA ONLINE, SL use
+            cookies that collect data relating to web usage analysis. These are
+            used to help improve customer service, measuring the use and
+            performance of the site, to optimize and customize it.
+          </p>
+          <p>
+            Our sites may also have links to social networks (such as Facebook
+            or Twitter). LEEOSMERCH, SL and VIZZMEDIA ONLINE, SL do not control
+            the cookies used by these external websites. For more information
+            about cookies from social networks or other external websites, we
+            advise you to review their own cookie policies.
+          </p>
+          <p>This website uses cookies as follows:</p>
+          <p>
+            Google Analytics Cookie: Google Analytics is a free service offered
+            by Google Inc. that collects information about the pages of the
+            website consulted, at what time, which Web browser, etc..
+            Subsequently, this information is sent to Google Inc. servers in the
+            United States.
+          </p>
+          <p>
+            Add-this" application cookies: Add-this is a free service that
+            allows you to share pages of this website by different means (email,
+            social networks, etc.). Add-this collects statistical information
+            about your browsing to provide advertising tailored to your browsing
+            habits on this website or elsewhere.
+          </p>
+        </div>
+        <div>
+          <p>1. HOW DO WE USE THE DIFFERENT TYPES OF COOKIES?</p>
+          <p>According to their purpose:</p>
+          <p>
+            Technical cookies: Technical cookies are those essential and
+            strictly necessary for the proper functioning of a web portal and
+            the use of the different options and services it offers. For
+            example, those used for session maintenance, response time
+            management, performance or validation of options, use of security
+            elements, sharing content with social networks, etc.
+          </p>
+          <p>
+            Personalization cookies: These cookies allow the user to specify or
+            customize some features of the general options of the website, for
+            example, define the language, regional settings or browser type.
+          </p>
+          <p>
+            Analytical Cookies: Analytical cookies are those used by our web
+            portal, to elaborate navigation profiles and to be able to know the
+            preferences of the users of the same in order to improve the offer
+            of products and services. For example, through an analytical cookie
+            would control the geographic areas of greatest interest to a user,
+            which is the most popular product, etc..
+          </p>
+          <p>
+            Advertising / advertising cookies: Advertising cookies allow the
+            management of advertising spaces based on specific criteria. For
+            example, the frequency of access, edited content, etc.. Advertising
+            cookies allow through the management of advertising to store
+            behavioral information through the observation of habits, studying
+            the accesses and forming a profile of user preferences, to offer
+            advertising related to the interests of your profile.
+          </p>
+          <p>According to deadline:</p>
+          <p>
+            Session cookies: Session cookies are those that last the time the
+            user is browsing the website and are deleted at the end.
+          </p>
+          <p>
+            Persistent cookies: These cookies remain stored in the user's
+            terminal, for a longer time, thus facilitating the control of the
+            chosen preferences without having to repeat certain parameters each
+            time the website is visited.
+          </p>
+        </div>
+        <div>
+          <p>
+            1. WHAT IF I DON'T WANT TO HAVE THESE COOKIES OR I DON'T DO ANYTHING
+            ABOUT IT?
+          </p>
+          <p>
+            To comply with current legislation, we have to ask your permission
+            to manage cookies. If you decide not to authorize the processing by
+            indicating non-conformity, we would only use technical cookies, as
+            they are essential for browsing our website. In this case, no
+            cookies are stored. In the case of continuing to browse our website
+            without refusing your authorization implies that you accept their
+            use.
+          </p>
+          <p>
+            Please note that if you reject or delete the Web browsing cookies,
+            we will not be able to maintain your preferences, some features of
+            the pages will not be operative, we will not be able to offer you
+            personalized services and each time you browse our website we will
+            have to request your authorization for the use of cookies again.
+          </p>
+          <p>
+            If you still decide to modify the configuration of your access to
+            the website, you should know that it is possible to delete cookies
+            or prevent this information from being recorded on your computer at
+            any time by modifying the configuration parameters of your browser:
+          </p>
+          <p>Internet Explorer cookie settings</p>
+          <p>Firefox cookie settings</p>
+          <p>Google Chrome Explorer cookie settings</p>
+          <p>Safari cookie settings</p>
+          <p>
+            These browsers are subject to change or modification, so we can not
+            guarantee that they are completely adjusted to the version of your
+            browser. You may also be using another browser not covered by these
+            links, such as Konqueror, Arora, Flock, etc. To avoid these
+            mismatches, you can access them directly from your browser's options
+            usually found in the Options menu, in the "Privacy" section. (Please
+            consult your browser's help for more information).
+          </p>
+        </div>
+        <div>
+          <p>1. WHERE CAN I GET MORE INFORMATION?</p>
+          <p>
+            For more information about the cookies policy, please contact us at
+            the following e-mail address: hello@ogsbrand.com
+          </p>
+        </div>
+      </StyledContainer>
+    </Container>
+  );
 };
 
 export default CookiePolicy;

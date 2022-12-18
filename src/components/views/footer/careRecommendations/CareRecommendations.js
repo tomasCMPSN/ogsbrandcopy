8caacc7130b9133da8fe25551ee457841643bf85
Tomas Campusano <tomascampusano.10@gmail.com>
2022-12-18 02:57:10 -0300

    Added: content in CareRecommendations. Ready for deploy

--- src/components/views/footer/careRecommendations/CareRecommendations.js
+++ src/components/views/footer/careRecommendations/CareRecommendations.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React from "react";
+import { Container } from "react-bootstrap";
+import { StyledContainer } from "../../auth/StyledContainer";
 
 const CareRecommendations = () => {
-    return (
-        <Container style={{ marginTop: "16vh" }}>
-            CareRecommendations
-        </Container>
-    );
+  return (
+    <Container style={{ marginTop: "16vh" }}>
+      <StyledContainer>
+        <div>
+          <h1 className="fw-bold pt-5">CARE RECOMMENDATIONS</h1>
+        </div>
+        <div className="py-4">
+          <p>
+            To make your garments and prints last as long as possible, we
+            recommend:
+          </p>
+          <ul>
+            <li>Machine wash at 40º maximum - Preferably in cold water</li>
+            <li>Do not dry clean</li>
+            <li>Do not use bleach</li>
+            <li>Washing and hanging inside out</li>
+            <li>Do not use dryer</li>
+            <li>Do not iron on the print</li>
+          </ul>
+        </div>
+      </StyledContainer>
+    </Container>
+  );
 };
 
 export default CareRecommendations;

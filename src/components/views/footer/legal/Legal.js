a1a554f185b3b98193529346edb9ebf7de5f83e4
Tomas Campusano <tomascampusano.10@gmail.com>
2022-12-18 02:23:30 -0300

    Added: content in Legal

--- src/components/views/footer/legal/Legal.js
+++ src/components/views/footer/legal/Legal.js
@@ -1,12 +1,89 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React from "react";
+import { Container } from "react-bootstrap";
+import { StyledContainer } from "../../auth/StyledContainer";
 
 const Legal = () => {
-    return (
-        <Container style={{ marginTop: "16vh" }}>
-            Legal
-        </Container>
-    );
+  return (
+    <Container style={{ marginTop: "16vh" }}>
+      <StyledContainer>
+        <div>
+          <h1 className="fw-bold pt-5">LEGAL</h1>
+        </div>
+        <div className="py-4">
+          <p>1. Corporate information</p>
+          <p>
+            In compliance with Law 34/2002, of July 11, 2002, of services of the
+            information society and electronic commerce, LEEOSMERCH, SL, SL and
+            VIZZMEDIA ONLINE, SL inform you that they are owners of the website:
+            ogsbrand.com, in accordance with the requirement of article 10 of
+            the mentioned Law, LEEOSMERCH, SL, SL and VIZZMEDIA ONLINE, SL ,
+            notify the following data: the owners of this website are
+            LEEOSMERCH, SL, SL with NIF/CIF: B67608836. Commercial Registry of
+            BARCELONA. Volume:47309. Folio:69,. Sheet: B 548103, and registered
+            office: DIPUTACIÓ, 321, postal code: 08009. City: BARCELONA,
+            Province: BARCELONA and VIZZMEDIA ONLINE, SL with NIF/CIF
+            B-66356361. BARCELONA Mercantile Registry, Volume 44460, Folio 1,
+            Section 8. Page B456653 and registered office: Pº de Gracia 25, 3rd
+            Floor, postal code: 080007. City BARCELONA, Province Barcelona. The
+            e-mail address for contact with the company is: hello@ogsbrand.com
+          </p>
+        </div>
+        <div>
+          <p>1. User and liability regime</p>
+          <p>
+            The navigation by access or use of the website and all subdomains
+            and directories included under it, as well as the services or
+            contents that can be obtained through it and all portals of own
+            domain under ownership of LEEOSMERCH, SL, SL and VIZZMEDIA ONLINE,
+            SL , grants the condition of User, by which you accept, when
+            navigating the ogsbrand.com page, all the conditions of use set
+            forth herein without prejudice to the application of the
+            corresponding regulations of mandatory legal compliance as the case
+            may be.
+          </p>
+          <p>
+            Therefore, if you do not agree with the considerations detailed in
+            this legal notice, please do not use the portal, since any use you
+            make of it or the services and contents included in it will imply
+            the acceptance of the legal terms included in this text.
+          </p>
+          <p>
+            The website of LEEOSMERCH, SL, SL and VIZZMEDIA ONLINE, SL provides
+            a wide range of information, services and data. The user assumes
+            responsibility for the correct use of the websites. This
+            responsibility extends to: The truthfulness and legality of the
+            information provided by the user in the forms issued by LEEOSMERCH,
+            SL, SL, SL and VIZZMEDIA ONLINE, SL to access certain content or
+            services offered by the website.
+          </p>
+          <p>
+            The use of the information, services and data offered by LEEOSMERCH,
+            SL, SL and VIZZMEDIA ONLINE, SL will not go against the provisions
+            of these conditions, the Law, morality, good customs or public order
+            or that, in any other case, may involve injury to the rights of
+            third parties or the operation of the websites. To this end, the
+            User shall refrain from using any of the contents for illegal
+            purposes or effects, prohibited in this text, harmful to the rights
+            and interests of others, or in any way damage, disable, overburden,
+            impair or impede the normal use of the contents, other Users or any
+            Internet user (hardware and software).
+          </p>
+          <p>
+            Users will be liable for damages of any kind that the company that
+            owns the website may suffer, directly or indirectly, as a result of
+            breach of any of the obligations arising from the use of the Web and
+            this privacy policy.
+          </p>
+          <p>
+            In particular, and by way of example only and not exhaustive, the
+            User undertakes not to transmit, disseminate or make available to
+            third parties information, data, content, messages, graphics,
+            drawings, sound and/or image files, photographs from the website.
+          </p>
+        </div>
+      </StyledContainer>
+    </Container>
+  );
 };
 
 export default Legal;

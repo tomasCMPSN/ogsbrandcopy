08944c226f592717701f39708d9d37b6d8f6daf3
Tomas Campusano <tomascampusano.10@gmail.com>
2022-12-18 01:07:31 -0300

    Added: content in TermsOfService

--- src/components/views/footer/termsOfService/TermsOfService.js
+++ src/components/views/footer/termsOfService/TermsOfService.js
@@ -1,12 +1,95 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React from "react";
+import { Container } from "react-bootstrap";
+import { StyledContainer } from "../../auth/StyledContainer";
 
 const TermsOfService = () => {
-    return (
-        <Container style={{ marginTop: "16vh" }}>
-            TermsOfService
-        </Container>
-    );
+  return (
+    <Container style={{ marginTop: "16vh" }}>
+      <StyledContainer>
+        <div>
+          <h1 className="fw-bold pt-5">TERMS OF SERVICE</h1>
+        </div>
+        <div className="py-4">
+          <p>
+            Through its WEBSITE; ogsbrand.com, of, LEEOSMERCH, SL AND VIZZMEDIA
+            ONLINE, SL, hereinafter the administrator of the website. Due to the
+            content and purpose of the WEBSITE, people who want information or
+            purchase the products offered on this website, must register in the
+            form of, USERS/CUSTOMERS, which is acquired by completing the form
+            of, REGISTRATION, your data will be incorporated into an automated
+            file for protection by the provisions of the Royal Decree-Law, of
+            July 27, for the adaptation of Spanish law to EU regulations, for
+            the application of, article, 13, of the General Data Protection
+            Regulation-UE-2016/679, of the Parliament and Council of Europe, of,
+            April 27, 2016, and the LOPD-3/2018, of guarantee of digital rights,
+            the Law applicable to Electronic Commerce, 34/2002, LSSI-CE, the
+            Royal Decree, 1906/1999, Telephone or Electronic Contracting and the
+            Law 26/1984, of July 19, Defence of Consumers and Users.
+          </p>
+          <p>
+            In attention to Article, 6, paragraph, f, of the RGPD-UE-2016/679,
+            and the LOPD-3/2018, of guarantee of digital rights, in its article,
+            7, exposes that the interests or fundamental rights and freedoms of
+            the data subject that require the protection of their personal data,
+            in particular when the data subject is a minor under 14, years of
+            age who cannot give their consent for an online business to collect
+            their personal data, being their legal representatives (parents or
+            guardians) who can do so on their behalf, except when the minor is
+            at least 16 years old, which will be lawful consent.
+          </p>
+          <p>Data Protection Officer: info@leeosmerch.com</p>
+        </div>
+        <div>
+          <p>1. SALES PRODUCT INFORMATION</p>
+          <p>
+            The WEBSITE: ogsbrand.com of which is Administrator, LEEOSMERCH, SL
+            and VIZZMEDIA ONLINE, SL of online sales, is a company dedicated to
+            the marketing of accessories.
+          </p>
+          <p>
+            For LEEOSMERCH, SL and VIZZMEDIA ONLINE, SL each user of this
+            website is a potential customer who deserves that our products live
+            up to their expectations.
+          </p>
+        </div>
+        <div>
+          <p>1. PRODUCT PRICES</p>
+          <p>
+            The prices shown on the WEBSITE include VAT on all products, when
+            applicable.
+          </p>
+        </div>
+        <div>
+          <p>1. ORDER PROCESSING</p>
+          <p>
+            Once the order is formalized, that is to say, with the acceptance of
+            the terms of sale and conditions of use and the confirmation of the
+            purchase process in the CART section, the administrator of the
+            ogsbrand.com website will always send an email to the USER/CUSTOMER,
+            confirming the purchase.
+          </p>
+        </div>
+        <div>
+          <p>1. PAYMENT METHOD</p>
+          <p>
+            The USER/CUSTOMER shall pay the amount at the time of ordering the
+            products purchased. The proof of purchase corresponding to the order
+            will be available and can be viewed on the WEBSITE.
+          </p>
+          <p>
+            The USER/CUSTOMER must pay the amount corresponding to his/her order
+            by credit card; VISA, MASTERCARD, 4B.
+          </p>
+          <p>
+            The USER/CUSTOMER must notify the administrator of the website of
+            any undue or fraudulent charge on the card used for payments, by
+            e-mail or telephone, in the shortest possible time so that the
+            administrator can take the appropriate steps.
+          </p>
+        </div>
+      </StyledContainer>
+    </Container>
+  );
 };
 
 export default TermsOfService;

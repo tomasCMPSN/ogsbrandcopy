2abf821fc2685c3f1cd66acc19778f645f17ffa8
Tomas Campusano <tomascampusano.10@gmail.com>
2022-12-18 00:01:47 -0300

    Added: content in Contact

--- src/components/views/footer/contact/Contact.js
+++ src/components/views/footer/contact/Contact.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React from "react";
+import { Container } from "react-bootstrap";
 
 const Contact = () => {
-    return (
-        <Container style={{ marginTop: "16vh" }}>
-            CONTACT
-        </Container>
-    );
+  return (
+    <Container style={{ marginTop: "16vh" }}>
+      <div>
+        <h1 className="fw-bold pt-5">CONTACT</h1>
+      </div>
+      <div className="mt-4">
+        <p>Write to us at hello@ogsbrand.com</p>
+        <p>
+          We will contact you within 48 hours (working hours) in order to solve
+          your doubts.
+        </p>
+      </div>
+    </Container>
+  );
 };
 
 export default Contact;

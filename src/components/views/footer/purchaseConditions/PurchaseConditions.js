fbe646508513d7a2d308f0cfccb2d715c6666f40
Tomas Campusano <tomascampusano.10@gmail.com>
2022-12-18 00:52:45 -0300

    Added: content in PurchaseConditions

--- src/components/views/footer/purchaseConditions/PurchaseConditions.js
+++ src/components/views/footer/purchaseConditions/PurchaseConditions.js
@@ -1,12 +1,230 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React from "react";
+import { Container } from "react-bootstrap";
+import { StyledContainer } from "../../auth/StyledContainer";
 
 const PurchaseConditions = () => {
-    return (
-        <Container style={{ marginTop: "16vh" }}>
-            PurchaseConditions
-        </Container>
-    );
+  return (
+    <Container style={{ marginTop: "16vh" }}>
+      <StyledContainer>
+        <div>
+          <h1 className="fw-bold pt-5">PURCHASE CONDITIONS</h1>
+        </div>
+        <div className="py-4">
+          <p>1. INFORMATION ABOUT THE WEB</p>
+          <p>
+            The website: ogsbrand.com of which is Administrator, LEEOSMERCH, SL
+            AND VIZZMEDIA ONLINE, SL, is a company dedicated to the marketing of
+            merchandising.
+          </p>
+          <p>
+            For any doubt, clarification, complaint or suggestion about the
+            purchase process, please contact:
+          </p>
+          <p>
+            LEEOSMERCH, SL, NIF: B67608836, Calle , Diputació, 321 , postal
+            code: 08009, Barcelona, or email: hello@ogsbrand.com
+          </p>
+          <p>
+            VIZZMEDIA ONLINE, SL, NIF: B66356361, Pº de Gracia 25, 3ª Planta,
+            postal code: 080007, Barcelona or email: hello@ogsbrand.com
+          </p>
+        </div>
+        <div>
+          <p>1. APPLICABLE REGULATION</p>
+          <p>
+            The conditions of purchase of our products are governed by these
+            contracting conditions that the buyer must accept, unconditionally,
+            prior to the purchase of the products. At all times, the language
+            used for the conclusion of the contract will be Spanish.
+          </p>
+          <p>
+            These terms and conditions have been drafted in accordance with the
+            provisions of Spanish and European consumer and user regulations
+            and, in particular, Law 3/2014, of 27 March, which amends the
+            revised text of the General Law for the Defense of Consumers and
+            Users and other complementary laws, approved by Royal Legislative
+            Decree 1/2007, of November 16, Law 34/2002, of July 11, on
+            information society services and electronic commerce and by
+            Regulation (EU) 524/2013 of the European Parliament and of the
+            Council, of May 21, 2013 , on online dispute resolution in consumer
+            matters.
+          </p>
+          <p>
+            Please consult, read and understand these terms and conditions
+            before purchasing any of our products.
+          </p>
+        </div>
+        <div>
+          <p>1. ORDER PROCESSING</p>
+          <p>
+            1) The customer selects the desired product by clicking on the "Add
+            to cart" button so that it is automatically added to the shopping
+            cart "SHOPPING CART".
+          </p>
+          <p>
+            2) Once you have selected the product, in the shopping cart you will
+            be able to modify the quantity of products you want, add more
+            products or continue with the payment.
+          </p>
+          <p>
+            3) Once you have selected the "FINISH ORDER" button, the WEBSITE
+            will ask you for a series of personal data in order to be able to
+            send the product and issue the corresponding invoice. You can also
+            select the box corresponding to the sending of commercial
+            communications or be reminded for future purchases in our store.
+          </p>
+          <p>
+            4) By clicking on the "Continue with shipments" button, the web will
+            show the data entered in the previous step and the different
+            shipping possibilities, and the customer will have to select one of
+            the possibilities. Once the shipping method has been entered, the
+            customer will continue to the corresponding payment page.
+          </p>
+        </div>
+        <div>
+          <p>1. PAYMENT METHOD</p>
+          <p>
+            In the last step of purchase, the USER/CUSTOMER, will pay the amount
+            of the purchase at the time of ordering the products purchased. To
+            do this the web will show our payment gateway to enter the necessary
+            data to proceed to the collection of the products. Please note that
+            the WEBSITE will not store any of the bank details entered, being
+            the payment gateway responsible for the management of their data.
+            For any questions in this regard please contact the responsible.
+          </p>
+          <p>
+            Once the purchase is completed, you will receive, within a maximum
+            period of 24 hours, in the email account entered, the proof of
+            purchase with all the details of the purchase. If you do not receive
+            this email, or you have detected any error in the data entered for
+            the purchase, please contact us.
+          </p>
+          <p>
+            The USER/CUSTOMER must notify the administrator of the website of
+            any undue or fraudulent charge on the card used for payments, by
+            e-mail or telephone, in the shortest possible time so that the
+            administrator can take the appropriate steps.
+          </p>
+        </div>
+        <div>
+          <p>1. RIGHT OF WITHDRAWAL</p>
+          <p>
+            All products purchased on the administrator's website can be
+            returned and refunded. The USER/CUSTOMER, has to communicate to the
+            administrator his/her intention to return the purchased product/s
+            within 15 calendar days from the reception of the product.
+          </p>
+          <p>
+            The website administrator will only accept returns that meet the
+            following requirements:
+          </p>
+          <p>
+            1. a) The product must be in the same condition in which it was
+            delivered and must retain its original packaging and labeling.
+          </p>
+          <p>
+            2. b) The shipment must be made using the same box or wrapping in
+            which it was received to protect the product.
+          </p>
+          <p>
+            3. c) In the event that this cannot be done with the packaging in
+            which it was delivered, the USER/CUSTOMER must return it in
+            protective packaging so that the product arrives at the website
+            administrator's company with the maximum possible guarantees.
+          </p>
+          <p>
+            4. d) A copy of the delivery note must be included inside the
+            packaging, marking the returned products and the reason for the
+            return.
+          </p>
+          <p>
+            If the reason for the return is attributable to the website
+            administrator (the product is incorrect or incomplete, it is not
+            what was ordered, etc.), the amount of the refund will be
+            reimbursed. If the reason is other, the cost of the return expenses
+            will be charged to the USER/CUSTOMER.
+          </p>
+          <p>To proceed with a return, the following steps must be followed:</p>
+          <p>
+            1. a) Inform within 24 hours and no later than 15 calendar days
+            after delivery, and indicate that the product is to be returned. The
+            information must be made to the e-mail address; hello@ogsbrand.com
+          </p>
+          <p>
+            2. b) The administrator of the website will inform the USER/CUSTOMER
+            of the address to which the product should be sent.
+          </p>
+          <p>
+            3. c) The USER/CUSTOMER, must return the products, within a maximum
+            period of 15 calendar days, by the same system that was delivered or
+            through a courier company of your choice. The cost of return shall
+            be borne by the USER/CUSTOMER.
+          </p>
+          <p>
+            4. d) Inform the website administrator of the system of the return
+            used, with date and time of the return.
+          </p>
+        </div>
+        <div>
+          <p>5. USER/CUSTOMER REFUNDS</p>
+          <p>
+            Once the right of withdrawal has been exercised and it has been
+            ascertained that the product is in the conditions established in
+            section "1. RIGHT OF WITHDRAWAL", the payment will be refunded to
+            the customer, excluding shipping and return costs, which will be
+            borne, at all times, by the customer.
+          </p>
+          <p>
+            The website administrator will manage the refund order under the
+            same system that was used for the payment. The application of the
+            refund on the account or card of the USER/CUSTOMER will depend on
+            the card and the issuing entity. The application period will be up
+            to 30 days for credit cards.
+          </p>
+        </div>
+        <div>
+          <p>1. TAXES AND FEES</p>
+          <p>
+            All our products shipped to European Union territory include VAT.
+            You will also be informed before the purchase of the price of
+            shipping the products.
+          </p>
+        </div>
+        <div>
+          <p>1. PRICE OF PRODUCTS</p>
+          <p>
+            The prices of the products offered on the Web are indicated in euros
+            (€).
+          </p>
+          <p>
+            The offers and promotions will be expressly indicated, understanding
+            that an excessively low price compared to other products or equal to
+            zero will be due to an error, so the final purchase process of the
+            product that has this error will not be carried out. In case of
+            being in this situation, we recommend the CUSTOMER to send the
+            incident to the following email: hello@ogsbrand.com
+          </p>
+        </div>
+        <div>
+          <p>1. ONLINE CONFLICT RESOLUTION:</p>
+          <p>
+            In compliance with European consumer law, we inform users of the
+            existence of a European dispute resolution platform for online
+            contracting. Thus, for the resolution of consumer disputes (pursuant
+            to Art. 14.1 of Regulation (EU) 524/2013), the European Commission
+            provides an online dispute resolution platform which is available at
+            the following link:
+          </p>
+          <a
+            href="https://ec.europa.eu/consumers/odr/main/index.cfm?event=main.home2.show&lng=ES"
+            target="_blank"
+          >
+            https://ec.europa.eu/consumers/odr/main/index.cfm?event=main.home2.show&lng=ES
+          </a>
+        </div>
+      </StyledContainer>
+    </Container>
+  );
 };
 
 export default PurchaseConditions;
